5e6b1de4d0e5f3daaee86df97c3cd5afd4a4060b
AlexGithubbb <31776215+AlexGithubbb@users.noreply.github.com>
2019-10-09 18:41:25 -0400

    Updates

---
+++ precache-manifest.dc06ac6fd3dddf18042bb4491575fe03.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1d7b2825dbd63467fa108ebbcb84a27",
+    "url": "/stockdemo/index.html"
+  },
+  {
+    "revision": "f24b97fc54c5c1795fdb",
+    "url": "/stockdemo/static/css/main.ba3ba950.chunk.css"
+  },
+  {
+    "revision": "6686fdfa3ecb24dad776",
+    "url": "/stockdemo/static/js/2.b1643a21.chunk.js"
+  },
+  {
+    "revision": "f24b97fc54c5c1795fdb",
+    "url": "/stockdemo/static/js/main.a96c212f.chunk.js"
+  },
+  {
+    "revision": "6f7786928dd802ae3924",
+    "url": "/stockdemo/static/js/runtime-main.957fa11f.js"
+  }
+]);
